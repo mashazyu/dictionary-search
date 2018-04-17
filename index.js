ea5d05c4a4b8d1f86e6bd697b1229ac9a6942284
mkz <mariya.z@gmail.com>
2018-04-17 17:40:01 +0200

    option to use dictionaries without api keys

--- index.js
+++ index.js
@@ -16,9 +16,125 @@ const textColors = ['gray', 'white']
 let textColorsIndex = 0
 
 const applyStyle = (text1, text2) => {
-
     console.log((text1.underline + text2)[textColors[textColorsIndex%2]])    
     textColorsIndex++
+}
+
+const lookupABBY = async (key) => {
+
+    const ABBYAuth = axios.create({
+        baseURL: ABBYAuthEndpointURL,
+        headers: {
+            "Authorization": `Basic ${key}`
+        }
+    })
+
+    const token = await ABBYAuth.post('/authenticate')
+
+    const agent = axios.create({
+        baseURL: ABBYEndpointURL,
+        headers: {
+            "Authorization": `Bearer ${token.data}`
+        }
+    })
+
+    const uri = url.format({
+        query: {
+            text: word,
+            srcLang: 1031,
+            dstLang: 1049       
+        }
+    })
+
+    const response = await agent.get(uri)
+        .catch(error => applyStyle('/// ABBY ', error))
+    if (response && response.data.Translation.Translation) {
+        const translation = response.data.Translation.Translation
+        applyStyle('/// ABBYTranslation ', translation)
+    }    
+
+}
+
+const lookupSystran = async (key) => {
+
+    const uri = url.format({
+        pathname: SystranEndpointURL,
+        query: {
+            key: key,
+            input: word,
+            target: 'en',
+            source: 'de'
+        }
+    })
+
+    const response = await axios.get(uri)
+        .catch(error => applyStyle('/// SystranTrans ', error))
+    if (response && response.data.outputs.length>0) {
+        const translation = response.data.outputs[0].output
+        applyStyle('/// SystranTranslation ', translation)
+    }
+
+}
+
+const lookupYandex = async (key) => {
+
+    const uri = url.format({
+        pathname: YandexEndpointURL,
+        query: {
+            key: key,
+            text: word,
+            lang: 'de-en'
+        }
+    })
+
+    const response = await axios.get(uri)
+        .catch(error => applyStyle('/// Yandex ', error))
+    if (response && response.data.def.length>0) {
+        const translation = response.data.def[0].tr[0].text
+        applyStyle('/// YandexTranslation ', translation)
+    }
+
+}
+
+const lookupGlosbe = async () => {
+
+    const uri = url.format({
+        pathname: GlosbeEndpointUrl, 
+        query: {
+            from: 'deu',
+            dest: 'eng',
+            format: 'json',
+            phrase: word,
+            pretty: true
+        }
+    })
+
+    const response = await axios.get(uri)
+        .catch(error => applyStyle('/// GlosbeTrans '.underline, error))
+    if (response && response.data.tuc.length>0) {
+        const translation = response.data.tuc[0].phrase.text
+        applyStyle('/// GlosbeTranslation ', translation)
+    }
+
+}
+
+const lookupOpenThesaurus = async () => {
+
+    const uri = url.format({
+        pathname: OpenThesaurusEndpointUrl,
+        query: {
+            q: word,
+            format: 'application/json'
+        }
+    })
+
+    const response = await axios.get(uri)
+        .catch(error => applyStyle('/// OpenThesaurus ', error))
+    if (response && response.data.synsets.length>0) {
+        const synonims = response.data.synsets[0].terms
+        synonims.forEach(syn => applyStyle('/// OpenThesaurusSynonims ', syn.term))
+    }
+
 }
 
 const main = async () => {
@@ -30,121 +146,24 @@ const main = async () => {
     const SystranKey = process.env.SYSTRAN_KEY
     const YandexKey = process.env.YANDEX_KEY
 
-    if (YandexKey===undefined || SystranKey=== undefined || ABBYKey) {
-        console.log('Please add ABBYKey, SystranKey, YandexKey variables to your environment.')
-        return
-    }
-
     let response
 
     console.log('We are looking for translation of word ')
 
     figlet(word, function(err, data) {
         if (err) {
-            console.log(word);
+            console.log(word).rainbow;
         }
         console.log(data.rainbow)
     })
     
-    const ABBYAuth = axios.create({
-        baseURL: ABBYAuthEndpointURL,
-        headers: {
-            "Authorization": `Basic ${ABBYKey}`
-        }
-    })
-
-    const ABBYToken = await ABBYAuth.post('/authenticate')
-
-    const ABBYAgent = axios.create({
-        baseURL: ABBYEndpointURL,
-        headers: {
-            "Authorization": `Bearer ${ABBYToken.data}`
-        }
-    })
-
-    const ABBYUri = url.format({
-        query: {
-            text: word,
-            srcLang: 1031,
-            dstLang: 1049       
-        }
-    })
-
-    response = await ABBYAgent.get(ABBYUri)
-        .catch(error => applyStyle('/// ABBY ', error))
-    if (response && response.data.Translation.Translation) {
-        const ABBYTranslation = response.data.Translation.Translation
-        applyStyle('/// ABBYTranslation ', ABBYTranslation)
-    }
-
-    const SystranURI = url.format({
-        pathname: SystranEndpointURL,
-        query: {
-            key: SystranKey,
-            input: word,
-            target: 'en',
-            source: 'de'
-        }
-    })
-
-    response = await axios.get(SystranURI)
-        .catch(error => applyStyle('/// SystranTrans ', error))
-    if (response && response.data.outputs.length>0) {
-        const SystranTranslation = response.data.outputs[0].output
-        applyStyle('/// SystranTranslation ', SystranTranslation)
-    }
-
-    const YandexURI = url.format({
-        pathname: YandexEndpointURL,
-        query: {
-            key: YandexKey,
-            text: word,
-            lang: 'de-en'
-        }
-    })
-
-    response = await axios.get(YandexURI)
-        .catch(error => applyStyle('/// Yandex ', error))
-    if (response && response.data.def.length>0) {
-        const YandexTranslation = response.data.def[0].tr[0].text
-        applyStyle('/// YandexTranslation ', YandexTranslation)
-    }
-
-    const GlosbeUri = url.format({
-        pathname: GlosbeEndpointUrl, 
-        query: {
-            from: 'deu',
-            dest: 'eng',
-            format: 'json',
-            phrase: word,
-            pretty: true
-        }
-    })
-
-    response = await axios.get(GlosbeUri)
-        .catch(error => applyStyle('/// GlosbeTrans '.underline, error))
-    if (response && response.data.tuc.length>0) {
-        const GlosbeTranslation = response.data.tuc[0].phrase.text
-        applyStyle('/// GlosbeTranslation ', GlosbeTranslation)
-    }
-
-    const OpenThesaurusUri = url.format({
-        pathname: OpenThesaurusEndpointUrl,
-        query: {
-            q: word,
-            format: 'application/json'
-        }
-    })
-
-    response = await axios.get(OpenThesaurusUri)
-        .catch(error => applyStyle('/// OpenThesaurus ', error))
-    if (response && response.data.synsets.length>0) {
-        const OpenThesaurusSynonims = response.data.synsets[0].terms
-        OpenThesaurusSynonims.forEach(syn => applyStyle('/// OpenThesaurusSynonims ', syn.term))
-    }
+    if (ABBYKey) await lookupABBY(ABBYKey)
+    if (SystranKey) await lookupSystran(SystranKey)
+    if (YandexKey) await lookupYandex(YandexKey)
+    await lookupGlosbe()
+    await lookupOpenThesaurus()
     
 }
 
- 
 main()
  
